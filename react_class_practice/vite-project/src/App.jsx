5eb389de07b131131a92135490b1a779538c2350
Sanskriti Vats <sanskriti2020vats@gmail.com>
2025-05-16 12:49:26 +0530

    react counter

--- react_class_practice/vite-project/src/App.jsx
+++ react_class_practice/vite-project/src/App.jsx
@@ -2,9 +2,28 @@ import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
+  const handleclick1 = () => {
+    setCount(count+1);
+  }
+  
+  const handleclick2 = () => {
+    setCount(count-1);
+  }
+
  return(
-   <h1>helllo world</h1>
- )
+<div>
+            <div>
+                {count}
+            </div>
+            <div className="buttons">
+                <button onClick={handleclick1}>
+                    Increment
+                </button>
+                <button onClick={handleclick2}>
+                    Decrement
+                </button>
+            </div>
+        </div> )
 }
 
 export default App
