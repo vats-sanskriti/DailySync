9e2a568c4b68e36b10a3d6fce146f4db6f104a60
Sanskriti Vats <sanskriti2020vats@gmail.com>
2025-06-06 17:56:47 +0530

    practice

--- react_class_practice/vite-project/src/App.jsx
+++ react_class_practice/vite-project/src/App.jsx
@@ -1,29 +1,40 @@
 import { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-  const handleclick1 = () => {
-    setCount(count+1);
-  }
+// function App() {
+//   const [count, setCount] = useState(0)
+//   const handleclick1 = () => {
+//     setCount(count+1);
+//   }
   
-  const handleclick2 = () => {
-    setCount(count-1);
-  }
+//   const handleclick2 = () => {
+//     setCount(count-1);
+//   }
 
- return(
-<div>
-            <div>
-                {count}
-            </div>
-            <div className="buttons">
-                <button onClick={handleclick1}>
-                    Increment
-                </button>
-                <button onClick={handleclick2}>
-                    Decrement
-                </button>
-            </div>
-        </div> )
+//  return(
+// <div>
+//             <div>
+//                 {count}
+//             </div>
+//             <div className="buttons">
+//                 <button onClick={handleclick1}>
+//                     Increment
+//                 </button>
+//                 <button onClick={handleclick2}>
+//                     Decrement
+//                 </button>
+//             </div>
+//         </div> )
+// }
+
+const name = "sanskriti";
+const number = 7;
+function App()
+{
+  return(
+    <div>
+      <h1>Hello {name}</h1>
+      <p>my lucky number is {number}</p>
+    </div>
+  )
 }
-
 export default App
